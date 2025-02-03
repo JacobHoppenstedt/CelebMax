d2a35935b4412f9e66da19910645f5baa2792b81
rohithv999 <73917116+rohithv999@users.noreply.github.com>
2025-02-03 16:09:57 -0500

    Folder for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>CelebMax</h1>
+     <h2>Find Your Look, Inspired By Stars</h2>
+    </div>
+  );
+}
+
+export default App;
