da439f33e0d4fabc76b537207856a31761faf4e3
d-dovale <ddovale2004@gmail.com>
2025-02-05 13:55:24 -0500

    Updated results page to have the proper top header, and changed some index styling to fit according to our design

--- frontend/src/Results_Page/resultspage.jsx
+++ frontend/src/Results_Page/resultspage.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from "./resultspage.module.css";
+import { useNavigate } from 'react-router-dom';
+import Logo from "../assets/logo.png";
+import Back from "../assets/Back.png";
 
 const ResultsPage = () => {
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate('/');
+    }
+
     return (
-        <div>
-            <h1>Results Page</h1>
-            <p>This is the results page.</p>
+        <div className={styles.resultsPage}>
+          <header className={styles.header}>
+            <img src={Logo} alt="Logo" className={styles.logo} />
+            <h1 className={styles.title}>Results Page</h1>
+            <button className={styles.goBackButton} onClick={handleGoBack}>
+              <img src={Back} alt="Back" className={styles.backicon}/>
+              Go Back
+            </button>
+          </header>
+          <p>This is the results page.</p>
         </div>
-    );
-};
+      );
+    };
 
 export default ResultsPage;
