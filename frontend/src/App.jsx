7bae25d2ffec64459170d72eb71f0adc7ff1a7d9
d-dovale <ddovale2004@gmail.com>
2025-03-30 21:38:56 -0400

    fixed import errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HairstyleCustomizer from './Hairstyle_Customizer_Page/hairstyle_customizer.jsx';
+import CelebSearchPage from "./CelebSearchPage/CelebSearchPage.jsx";
 import LandingPage from './Landing_Page/landingpage.jsx';
 import ResultsPage from './Results_Page/resultspage.jsx';
 
